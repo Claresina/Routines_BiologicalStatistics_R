ae249716ba7a52add356002961190340d1f6d3f3
Clara Mitre do Prado <54377675+Claresina@users.noreply.github.com>
2019-08-21 20:14:11 -0300

    Add files via upload

---
+++ Pacotes de Análises Biológicas Diversos.R
@@ -0,0 +1,68 @@
+######################################################################################
+##########################         LISTA DE PACOTES         ##########################
+####################         ANÁLISES BIOLÓGICAS DIVERSAS         ####################
+##################         CLARA MITRE DO PRADO - @claresina        ##################
+#######################              21/08/2019               ########################
+######################################################################################
+
+
+#--------------------- PACOTES DE ANÁLISES BIOLÓGICAS DIVERSAS ----------------------#
+
+  
+#Esta é uma lista dos pacotes de interesse biológico e acadêmico dentro das áreas de
+#ECOLOGIA, CITOTAXONOMIA, EVOLUÇÃO, GENÉTICA E MELHORAMENTO DE PLANTAS.
+
+
+#Para instalar os pacotes, siga o caminho:
+install.packages("NOME DO PACOTE") #Instalar pacotes
+library("NOME DO PACOTE") #Carregar os pacotes
+
+
+#Genética e Melhoramento de Plantas:
+
+install.packages("ExpDes.pt")
+library("ExpDes.pt")
+
+install.packages("lme4")
+library("lme4")
+
+install.packages("lmerTest")
+library("lmerTest")
+
+install.packages("laercio")
+library("laercio")
+
+install.packages("fBasics")
+library (fbasics)
+
+install.packages("ggplot2")
+library (ggplot2)
+
+install.packages("car")
+library (car)
+
+install.packages("ExpDes")
+library (ExpDes)
+
+install.packages("agricolae")
+library (agricolae)
+
+install.packages("asbio")
+library (asbio)
+
+install.packages("latticeExtra")
+library (latticeExtra)
+
+
+#Evolução e Filogenia:
+
+install.packages("phytools")
+library("phytools")
+
+install.packages("ape")
+library("ape")
+
+
+
+#-------------------------------------- FIM ----------------------------------------#
+
